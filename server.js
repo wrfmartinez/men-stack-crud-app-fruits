31bebaac698b56a5e8b172cc22797603046f6530
wrfmartinez <wrmartinez709@gmail.com>
2024-04-25 10:00:13 -0400

    Setup connection to database

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+// dotenv package setup
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Connection to MongoDB database
+const mongoose = require('mongoose');
+const connect = () => {
+  mongoose.connect(process.env.MONGODB_URI);
+  console.log('Connected to MongoDB');
+}
+
 app.get('/', (req, res) => {
   res.render('index.ejs');
 })
@@ -8,3 +19,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+connect();
