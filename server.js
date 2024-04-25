3ffa98e79bd42bcbcfc191287086473453191b07
wrfmartinez <wrmartinez709@gmail.com>
2024-04-25 10:18:52 -0400

    Log connection confirmation to terminal with app name

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const app = express();
-
-// dotenv package setup
 const dotenv = require('dotenv');
-dotenv.config();
-
-// Connection to MongoDB database
 const mongoose = require('mongoose');
-const connect = () => {
-  mongoose.connect(process.env.MONGODB_URI);
-  console.log('Connected to MongoDB');
-}
+
+const app = express();
+// Loads the environment variables from .env
+dotenv.config();
+// Connection to MongoDB database
+mongoose.connect(process.env.MONGODB_URI);
+// Log connection confirmation to terminal
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+})
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
@@ -19,5 +19,3 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
-
-connect();
