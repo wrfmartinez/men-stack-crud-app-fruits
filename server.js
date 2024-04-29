c2b23cb9d03078726deb047991eb1636884be6e2
wrfmartinez <wrmartinez709@gmail.com>
2024-04-29 09:12:04 -0400

    Create show route for fruits

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const Fruit = require('./models/fruit');
 
 /* ---- CONFIGURATIONS ---- */
 const app = express();
-/* Parses incoming request bodies, extracts form data
-   then converts it into an object */
+// Parses incoming request bodies, extracts form data then converts it into an object
 app.use(express.urlencoded({ extended: false }));
 // Loads the environment variables from .env
 dotenv.config();
@@ -19,32 +18,40 @@ mongoose.connection.on('connected', () => {
 });
 
 /* ---- ROUTES ---- */
+// Renders the landing page
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
-// This route presents the user with a form
+// Method:GET Endpoint/URI:(/fruits/new) Presents the user with a form to add a fruit
 app.get('/fruits/new', (req, res) => {
   res.render('fruits/new.ejs');
 });
 
-// This route retrieves all created fruits
+// Method:GET Endpoint/URI:(/fruits/:fruitId) Renders a show page for a specific fruit
+app.get('/fruits/:fruitId', async (req, res) => {
+  // mongoose will find the fruit based on the id value in the param within the route
+  // index.ejs has links referencing a route to '/fruits/fruit._id
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render('fruits/show.ejs', { fruit: foundFruit });
+});
+
+// Method:GET Endpoint/URI:(/fruits) Read all fruits and passes it to the index.ejs in the views/fruits directory
 app.get('/fruits', async (req, res) => {
   const allFruits = await Fruit.find();
   res.render('fruits/index.ejs', { fruits: allFruits });
 });
 
-// This route creates a fruit
+// Method:POST Endpoint/URI:(/fruits) Create a new fruit then redirects to any endpoint of your choosing. In this case, /fruits/new route
 app.post('/fruits', async (req, res) => {
   if (req.body.isReadyToEat === 'on') {
     req.body.isReadyToEat = true;
   } else {
     req.body.isReadyToEat = false;
   }
-  /* Creates a fruit object within the database based on
-     user form submission data */
+  // Creates a fruit object within the database based on user form submission data
   await Fruit.create(req.body);
-  res.redirect('/fruits/new');
+  res.redirect('/fruits');
 });
 
 app.listen(3000, () => {
